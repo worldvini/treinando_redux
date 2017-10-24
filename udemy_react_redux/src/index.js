71823ae51fc87ad2778f5630fd99eaab8edd0c7b
Vinicius da Silva Fleury <worldvini@hotmail.com>
2017-10-23 23:32:24 -0200

    começando aulas no udemy sobre react-redux

---
+++ udemy_react_redux/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyC5GOO3dQuYItf-kbAvMtkv4P9Zv3HRhBs'
+
+const App = () => {
+    return <div> hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
