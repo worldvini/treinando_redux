3886a40389884f45cbb2bc5be38f50efff550194
Vinicius da Silva Fleury <worldvini@hotmail.com>
2017-11-01 14:42:41 -0200

    aula 13 to 19

--- udemy_react_redux/src/index.js
+++ udemy_react_redux/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyC5GOO3dQuYItf-kbAvMtkv4P9Zv3HRhBs'
 
 const App = () => {
-    return <div> hi!</div>;
+    return <div> 
+                <SearchBar />
+            </div>; 
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
