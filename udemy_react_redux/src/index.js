72bab3f00da6e7e2437ee5f412aee53eef28b3aa
Vinicius da Silva Fleury <worldvini@hotmail.com>
2017-12-04 22:34:00 -0200

    adicionando videos

--- udemy_react_redux/src/index.js
+++ udemy_react_redux/src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar'
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyC5GOO3dQuYItf-kbAvMtkv4P9Zv3HRhBs'
+const API_KEY = 'AIzaSyC5GOO3dQuYItf-kbAvMtkv4P9Zv3HRhBs';
 
-const App = () => {
-    return <div> 
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render (){
+        return (
+            <div> 
                 <SearchBar />
-            </div>; 
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
